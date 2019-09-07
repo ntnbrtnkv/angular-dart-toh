13368b16e58a091e3ccb6345b65dfbbacb3726d6
Anton Bortnikov <6088046+ntnbrtnkv@users.noreply.github.com>
2019-09-07 20:48:01 +0500

    feat: add dashboard route with names of heroes

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -2,6 +2,7 @@ import 'package:angular_router/angular_router.dart';
 
 import 'route_paths.dart';
 import 'hero_list_component.template.dart' as hero_list_template;
+import 'dashboard_component.template.dart' as dashboard_template;
 
 class Routes {
   static final heroes = RouteDefinition(
@@ -9,7 +10,17 @@ class Routes {
     component: hero_list_template.HeroListComponentNgFactory,
   );
 
+  static final dashboard = RouteDefinition(
+    routePath: RoutePaths.dashboard,
+    component: dashboard_template.DashboardComponentNgFactory,
+  );
+
   static final all = <RouteDefinition>[
     heroes,
+    dashboard,
+    RouteDefinition.redirect(
+      path: '',
+      redirectTo: RoutePaths.dashboard.toUrl(),
+    ),
   ];
 }
